70c3f3a4a186697d070dac9f226ac47e7ac97eb2
Gary Gillespie <gary@banntech.co.uk>
2019-04-02 20:34:46 +0100

    Retrieve race content nodes, refactor structure

--- RacingWebScrape/Program.cs
+++ RacingWebScrape/Program.cs
@@ -10,7 +10,15 @@ namespace RacingWebScrape
 {
     class Program
     {
-        private static object tags;
+        // Processed content
+        private static List<string> DailyMeetingTitles {get; set;}
+
+
+
+        //Raw html node collections, unprocessed
+        private static HtmlNodeCollection DailyMeetingTitleNodes {get; set;}
+        private static List<HtmlNode> DailyMeetingContentNodes {get; set;}
+
 
         static void Main(string[] args)
         {
@@ -26,43 +34,19 @@ namespace RacingWebScrape
             HtmlDocument pageDocument = new HtmlDocument();
             pageDocument.LoadHtml(pageContents);
 
-            string[] dailyMeeting = new string[pageDocument.DocumentNode.SelectSingleNode("(//div[contains(@class,'w-results-holder')]//a)").ChildNodes.Count];
-            var headlineText = pageDocument.DocumentNode.SelectSingleNode("(//div[contains(@class,'w-results-holder')]//a)[1]").InnerText;
-            var dailyMeetingNodes = pageDocument.DocumentNode.SelectNodes("(//div[contains(@class,'w-results-holder')][1]//a)");
+            //meeting titles are used to retrieve the id's for retrieving content nodes
+            DailyMeetingTitleNodes = pageDocument.DocumentNode.SelectNodes("(//div[contains(@class,'w-results-holder')][1]//a)");
+            
+            RetrieveDailyMeetingContentNodes(ref pageDocument);
 
-            //Daily Meetings List - Refactor into method
-            var dailyMeetingTitles = new List<string>();
+            PrintDailyMeetingTitles();
+            PrintDailyResults();
 
-            foreach (var meeting in dailyMeetingNodes)
-            {
-                if(meeting.GetClasses().Count() == 1)
-                {
-                    if(meeting.GetClasses().ElementAt(0) != "")
-                    {
-                        if (!dailyMeetingTitles.Contains(meeting.InnerHtml))
-                        {
-                            var s = meeting.InnerHtml.ToString();
-                            s = s.Replace(">", "");
-                            dailyMeetingTitles.Add(s);
-                        }
-                    }
-                }
-            }
-
-            //Intro
-            Console.WriteLine("There are : " + dailyMeetingTitles.Count + " Meetings In The UK Today");
-
-            int counter = 0;
-            foreach(var meeting in dailyMeetingTitles)
-            {
-                counter++;
-                Console.WriteLine(counter+". " + meeting);
-            }
-
+            Console.WriteLine("Daily Meeting content nodes: " + DailyMeetingContentNodes.Count);
             Console.ReadLine();
         }
 
-        private static void PrintClasses(HtmlNode node)
+        private static void PrintNodeClasses(HtmlNode node)
         {
             var classes = node.GetClasses();
             foreach(var c in classes)
@@ -70,6 +54,61 @@ namespace RacingWebScrape
                 Console.WriteLine(c.ToString());
             }
         }
+
+        private static void RetrieveDailyMeetingContentNodes(ref HtmlDocument pageDocument)
+        {
+            //Daily Meetings Titles
+            var dailyMeetingTitles = new List<string>();
+            var dailyMeetingContentNodes = new List<HtmlNode>();
+
+            foreach (var meeting in DailyMeetingTitleNodes)
+            {
+                if(meeting.GetClasses().Count() == 1)
+                {
+                    if(meeting.GetClasses().ElementAt(0) != "")
+                    {
+                        if (!dailyMeetingTitles.Contains(meeting.InnerHtml))
+                        {
+                            //Process title
+                            var s = meeting.InnerHtml.ToString();
+                            s = s.Replace(">", "");
+                            dailyMeetingTitles.Add(s);
+                            
+                            //Process content Id
+                            var meetingContentId = (meeting.Attributes["href"].Value + "-results");
+                            meetingContentId = meetingContentId.Remove(0,1);
+                            var meetingContentNode = (pageDocument.DocumentNode.SelectSingleNode("//*[@id=\"" + meetingContentId + "\"]"));
+                            if(meetingContentNode != null)
+                            {
+                                dailyMeetingContentNodes.Add(meetingContentNode);
+                            }
+                        }
+                    }
+                }
+            }
+            DailyMeetingContentNodes = dailyMeetingContentNodes;
+            DailyMeetingTitles = dailyMeetingTitles;
+        }
+
+        private static void PrintDailyMeetingTitles()
+        {
+            Console.WriteLine("There are " + DailyMeetingTitles.Count + " Meetings In The UK Today");
+
+            int counter = 0;
+            foreach(var meeting in DailyMeetingTitles)
+            {
+                counter++;
+                Console.WriteLine(counter+". " + meeting);
+            }
+        }
+
+        private static void PrintDailyResults()
+        {
+            
+
+        }
+
+
 
     }
 }
