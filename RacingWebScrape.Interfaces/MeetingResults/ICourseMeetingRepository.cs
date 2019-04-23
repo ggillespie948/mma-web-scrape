ab772ab1c64eda326485f1ecca96ec7bbd3f7b7e
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-04-23 22:21:47 +0100

    Add course meeting interface + repository

---
+++ RacingWebScrape.Interfaces/MeetingResults/ICourseMeetingRepository.cs
@@ -0,0 +1,21 @@
+﻿using RacingWebScrape.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RacingWebScrape.Interfaces.MeetingResults
+{
+    public interface ICourseMeetingRepository
+    {
+        void Add(CourseMeeting courseMeeting);
+        void Update(CourseMeeting courseMeeting);
+        void Delete(CourseMeeting courseMeeting);
+
+        IEnumerable<CourseMeeting> GetTodaysMeetings();
+
+        CourseMeeting Get(int id);
+        IEnumerable<CourseMeeting> Get();
+
+        bool DoesCourseMeetingExist(CourseMeeting courseMeeting);
+    }
+}
