ec15cdcc7d05af366d4f8c5d19ec1a0896e6d282
Gary Gillespie <gary@banntech.co.uk>
2019-04-23 15:39:14 +0100

    Implement unit of work instance, init course repository

---
+++ RacingWebScrape.UnitOfWork/RacingUnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using RacingWebScrape.Db;
+using RacingWebScrape.Interfaces;
+using RacingWebScrape.Repositories;
+
+namespace RacingWebScrape.UnitOfWork
+{
+    public class UnitOfWork : IRacingUnitOfWork
+    {
+        private readonly RacingDbContext _context;
+
+        public UnitOfWork(RacingDbContext context)
+        {
+            _context = context;
+            Courses = new CourseRepository(context);
+        }
+
+        public ICourseRepository Courses {get; set;}
+
+        public void Complete()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CompleteAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
