e0afc95ee1b65b7d1efd74261b08a4a0c65887ee
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-05-02 10:49:28 +0100

    Add jockey model

---
+++ RacingWebScrape.Models/Jockeys/Jockey.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RacingWebScrape.Models.Jockeys
+{
+    public class Jockey
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ResultName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Surname { get; set; }
+
+        public ICollection<ResultEntry> ResultHistory {get; set;}
+    }
+}
