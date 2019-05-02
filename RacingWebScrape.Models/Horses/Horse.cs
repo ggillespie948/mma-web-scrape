607de9a2d57830b9ff9d5faf5a4b0e3cc7281183
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-05-02 10:49:08 +0100

    add horse model

---
+++ RacingWebScrape.Models/Horses/Horse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RacingWebScrape.Models.Horses
+{
+    public class Horse
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+
+        public ICollection<ResultEntry> ResultHistory {get; set;}
+
+    }
+}
