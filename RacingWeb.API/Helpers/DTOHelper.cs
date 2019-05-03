b664399ffcb01a4ea053541536ee35029d4c2274
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-05-03 13:30:18 +0100

    Add DTO helper + dto classes for meeting result / result entry

---
+++ RacingWeb.API/Helpers/DTOHelper.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using RacingWeb.API.DTOs;
+using RacingWebScrape.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RacingWeb.API.Helpers
+{
+    public static class DTOHelper
+    {
+        public static void ConvertToDTo(Object obj, ref Mapper mapper)
+        {
+            switch (obj)
+            {
+                case MeetingResult meetingResult: return;
+                case MeetingResult meetingResults: return;
+                case ResultEntry meetingResult: return;
+            }
+
+
+        }
+
+        private static IEnumerable<MeetingResultDTO> MapMeetingResultsDTO(IEnumerable<MeetingResult> meetingResults, ref IMapper mapper)
+        {
+            
+
+            foreach(var meetingResult in meetingResults)
+            {
+                mapper.Map<MeetingResult>
+                
+            }
+
+        }
+
+        private static void MapMeetingResultDTO()
+        {
+
+        }
+    }
