6f6a6743cd62627eebc9c7d1669902d56d03389c
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-05-03 13:30:51 +0100

    Add automapper + mapping profile

---
+++ RacingWeb.API/AutoMapperProfiles/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using RacingWeb.API.DTOs;
+using RacingWebScrape.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RacingWeb.API.MappingProfile
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<MeetingResult, MeetingResultDTO>().ReverseMap();
+            CreateMap<ResultEntry, ResultEntryDTO>().ReverseMap();
+        }
+    }
+}
