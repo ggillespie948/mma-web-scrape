f527f73334967f398ebf82403fbc7be52aab032d
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-05-03 14:42:25 +0100

    add result entry/ meeting result DTO content

--- RacingWeb.API/DTOs/MeetingResultDTO.cs
+++ RacingWeb.API/DTOs/MeetingResultDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RacingWebScrape.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,30 @@ namespace RacingWeb.API.DTOs
 {
     public class MeetingResultDTO
     {
+        public int Id { get; set; }
+
+        public DateTime RaceTime { get; set; }
+
+        public string RaceTitle { get; set; }
+
+        public int CourseMeetingId { get; set; }
+        public CourseMeeting CourseMeeting { get; set; }
+
+        /// <summary>
+        /// Object containing each horse, position, jockey, trainer, price
+        /// </summary>
+        public virtual IList<ResultEntryDTO> ResultEntries { get; set; }
+
+        /// <summary>
+        /// String containing information of number of runners, name of non-runners
+        /// </summary>
+        public string RunnerInformation { get; set; }
+
+        public string WinningJockey { get; set; }
+
+        public string WinningTrainer { get; set; }
+
+        //Race number in meeting result
+        public int RaceNumber { get; set; }
     }
 }
--- RacingWeb.API/DTOs/ResultEntryDTO.cs
+++ RacingWeb.API/DTOs/ResultEntryDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RacingWebScrape.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,23 @@ namespace RacingWeb.API.DTOs
 {
     public class ResultEntryDTO
     {
+        public int Id { get; set; } //remove?
+
+        public int MeetingResultId { get; set; } //remove?
+        public MeetingResult MeetingResult { get; set; }
+
+        public string Place { get; set; } 
+
+        public string HorseName { get; set; }
+
+        public string HorseNumber { get; set; }
+
+        public string JockeyName { get; set; }
+
+        public string TrainerName { get; set; }
+
+        public string PriceFraction { get; set; }
+
+        public decimal PriceDecimal { get; set; }
     }
 }
