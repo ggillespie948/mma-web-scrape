b5a5ac5bbb0190cf00c511b2d9e8cb150ad2683b
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-05-21 13:34:07 +0100

    add unit of work pattern

---
+++ MMAWeb.UnitOfWork/IMMAUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMAWeb.UnitOfWork
+{
+    public interface IMMAUnitOfWork
+    {
+        void Complete();
+        Task CompleteAsync();
+    }
+}
---
+++ MMAWeb.UnitOfWork/MMAUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMAWeb.UnitOfWork
+{
+    public class UnitOfWork 
+    {
+        //private readonly MMADbContext _context;
+        //public UnitOfWork(MMADbContext context)
+        //{
+        //    _context = context;
+        //}
+
+        //public void Complete()
+        //{
+        //    _context.SaveChanges();
+        //}
+
+        //public async Task CompleteAsync()
+        //{
+        //    await _context.SaveChangesAsync();
+        //}
+    }
+}
