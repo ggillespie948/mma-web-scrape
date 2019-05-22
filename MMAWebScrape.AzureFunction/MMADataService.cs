403a183890b68971ac02411ede485132afa6af72
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-05-22 20:42:18 +0100

    add data service layer for mma data

---
+++ MMAWebScrape.AzureFunction/MMADataService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MMAWeb.Db;
+using MMAWeb.Models.PromotionMeetings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MMAWebScrape.AzureFunction
+{
+    public class MMADataService
+    {
+        public bool DoesPromotionMeetingExist(PromotionMeeting promotionMeeting,ref MMADbContext context)
+        {
+            if (context.PromotionMeetings.Where(i => i.Date == promotionMeeting.Date).Any())
+                return true;
+            else
+                return false;
+        }
+
+        public bool AddPromotionMeeting(PromotionMeeting promotionMeeting, ref MMADbContext context)
+        {
+            context.Add(promotionMeeting);
+            return false;
+        }
+
+        public bool UpdatePromotionMeeting(PromotionMeeting promotionMeeting, ref MMADbContext context)
+        {
+            context.Entry(promotionMeeting).State = EntityState.Modified;
+            return true;
+        }
+
+        public PromotionMeeting GetPromotionMeetingByDate(DateTime dateTime, int promotionId, ref MMADbContext context)
+        {
+            return context.PromotionMeetings.Where(i => i.Date == dateTime && i.PromotionId == promotionId).FirstOrDefault();
+        }
+
+        public void SaveChanges(ref MMADbContext context)
+        {
+            context.SaveChanges();
+        }
+
+    }
+}
