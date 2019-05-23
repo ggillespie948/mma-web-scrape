7cd8f8825f835b7c0b7ea641d36d34dfff007730
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-05-23 01:02:19 +0100

    add webscrap start up file for dependency injection config

---
+++ MMAWebScrape.AzureFunction/WebScrapeStartup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using MMAWeb.Db;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+[assembly: FunctionsStartup(typeof(RacingWeb.WebScrapeAzureFunction.WebScrapeStartup))]
+namespace RacingWeb.WebScrapeAzureFunction
+{
+    public class WebScrapeStartup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+
+            builder.Services
+                .AddLogging()
+                .AddDbContext<MMADbContext>()
+                .BuildServiceProvider();
+
+        }
+    }
+}
