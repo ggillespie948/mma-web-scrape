a839dedb2e6204726f563f23300b9c70e59ee981
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-05-23 01:02:53 +0100

    make MMA Data Serivce a static class

--- MMAWebScrape.AzureFunction/MMADataService.cs
+++ MMAWebScrape.AzureFunction/MMADataService.cs
@@ -8,34 +8,34 @@ using System.Text;
 
 namespace MMAWebScrape.AzureFunction
 {
-    public class MMADataService
+    public static class MMADataService
     {
-        public bool DoesPromotionMeetingExist(PromotionMeeting promotionMeeting,ref MMADbContext context)
+        public static bool DoesPromotionMeetingExist(DateTime date, int promotionId, ref MMADbContext context)
         {
-            if (context.PromotionMeetings.Where(i => i.Date == promotionMeeting.Date).Any())
+            if (context.PromotionMeetings.Where(i => i.Date == date && i.PromotionId == promotionId).Any())
                 return true;
             else
                 return false;
         }
 
-        public bool AddPromotionMeeting(PromotionMeeting promotionMeeting, ref MMADbContext context)
+        public static bool AddPromotionMeeting(PromotionMeeting promotionMeeting, ref MMADbContext context)
         {
             context.Add(promotionMeeting);
             return false;
         }
 
-        public bool UpdatePromotionMeeting(PromotionMeeting promotionMeeting, ref MMADbContext context)
+        public static bool UpdatePromotionMeeting(PromotionMeeting promotionMeeting, ref MMADbContext context)
         {
             context.Entry(promotionMeeting).State = EntityState.Modified;
             return true;
         }
 
-        public PromotionMeeting GetPromotionMeetingByDate(DateTime dateTime, int promotionId, ref MMADbContext context)
+        public static PromotionMeeting GetPromotionMeetingByDate(DateTime dateTime, int promotionId, ref MMADbContext context)
         {
             return context.PromotionMeetings.Where(i => i.Date == dateTime && i.PromotionId == promotionId).FirstOrDefault();
         }
 
-        public void SaveChanges(ref MMADbContext context)
+        public static void SaveChanges(ref MMADbContext context)
         {
             context.SaveChanges();
         }
