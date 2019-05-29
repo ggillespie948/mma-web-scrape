9390d5b0913886cedee3d7736be3eb0b29ed3174
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-05-29 18:33:57 +0100

    add start up file for alexa azure function with db injection

---
+++ AlexaFunctionApp/AlexaFunctionStartup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using MMAWeb.Db;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+[assembly: FunctionsStartup(typeof(RacingWeb.AlexaAzureFunction.AlexaFunctionStartup))]
+namespace RacingWeb.AlexaAzureFunction
+{
+    public class AlexaFunctionStartup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services
+                .AddLogging()
+                .AddDbContext<MMADbContext>()
+                .BuildServiceProvider();
+        }
+    }
+}
