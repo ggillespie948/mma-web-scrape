74d3e5f955b10908c60648d7dc61254b81805e12
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-06-02 19:45:11 +0100

    migration for fight result fight number entry

--- MMAWeb.Db/Migrations/20190602163359_addfightno.Designer.cs
+++ MMAWeb.Db/Migrations/20190602163359_addfightno.Designer.cs
@@ -4,14 +4,16 @@ using MMAWeb.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MMAWeb.Db.Migrations
 {
     [DbContext(typeof(MMADbContext))]
-    partial class MMADbContextModelSnapshot : ModelSnapshot
+    [Migration("20190602163359_addfightno")]
+    partial class addfightno
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
